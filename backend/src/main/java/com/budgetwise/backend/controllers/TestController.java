f10d7fd497d72d879bdaf792d125b1b808324199
Nuwan Samarasinghe <nuwansamarasinghe100@gmail.com>
2025-06-23 07:06:34 +0100

    git ignore changes and formatting

--- backend/src/main/java/com/budgetwise/backend/controllers/TestController.java
+++ backend/src/main/java/com/budgetwise/backend/controllers/TestController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-    
-    @GetMapping("/test")
-    public String get(){
-        return "OK_TEST";
-    }
+
+	@GetMapping("/test")
+	public String get() {
+		return "OK_TEST";
+	}
 }
