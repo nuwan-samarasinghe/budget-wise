e61fe94cfc98fc24c59fb943500ba5e59937b8bf
Nuwan Samarasinghe <nuwansamarasinghe100@gmail.com>
2025-07-14 07:32:30 +0100

    Frontend: add material tailwind and other configs to setup frontend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react()
+  ],
+  css: {
+    postcss: { plugins: [tailwindcss()] }
+  },
+  server: {
+    host: true,
+    port: 5173,
+    watch: { usePolling: true }
+  },
 });
